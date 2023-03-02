ccf7a0963008e0b13c3dd0b04addd76c8426f5ba
Gokhan Ozdemir <77547072+gokhannozzdemir@users.noreply.github.com>
2023-03-02 23:44:19 +0300

    Update Authentication.java
    removed auth keys

--- RestAssured/src/test/java/Config/Authentication.java
+++ RestAssured/src/test/java/Config/Authentication.java
@@ -15,18 +15,16 @@ import static org.hamcrest.Matchers.*;
 import java.lang.*;
 
 public class Authentication {
-    //token = DeFacto
 
-
-    public String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjaGFudElkIjoiYzgwYjdkOTctMDY1OC00ZWZlLTkzYjEtYjJmMmFhOWY3YjdlIiwiVXNlcm5hbWUiOiJEZUZhY3RvIiwiaXAiOiIxODguMTE5LjM5LjE0MyIsImhiYWRtaW4iOiJnb2toYW4ub3pkZW1pciIsImFkbWluSWQiOiJlMDJhNmZlYy0yODBlLTRhMTYtYWRhMC1iNjdlZTllNjIxMWUiLCJpYXQiOjE2Nzc1Njc2MzYsImlzcyI6ImVudjpwcm9kdWN0aW9uIiwidXNlcklkIjoiYzg1MmUzNTAtOWQ2My00MzQ4LTk3N2EtOWM0OWM1YWU5MWFmIiwiZXhwIjoxNjc3NjU0MDM2fQ.krsUtFs_r5I-3pXUER-KS3Y6ZiS7nrW-girUPQpUfdw";
+    public String token = "";
 //
-//    public String username = "gokhan.ozdemir";
+//    public String username = "***********";
 //    public String password = "***********";
 
 
     //public String jirenPROD = System.getenv("jirenPROD");
+    
     public String jirenPROD = "http://localhost:3000";
-    public String localProd = "http://localhost:3000";
 
 
 }
